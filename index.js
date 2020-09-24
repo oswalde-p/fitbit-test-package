2fa60184d99d11efa0769d075dcd696e52efd063
jason <j.s.pursey@gmail.com>
2020-09-24 15:24:05 +1000

    Add a library import

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import { me as device } from "device";
+
+
 const myFunction = function() {
-    console.log('Inside the package')
+    console.log("Inside the package");
+    console.log("type:             " + device.type);
 }
 
 export {
